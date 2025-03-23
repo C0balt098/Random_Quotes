68572d5b5d7f8e7f53941458114f665778526e19
YarovoiMykola <yrovau3@gmail.com>
2025-03-23 20:19:37 +0200

    The theme was changed to dark, the quotes array was expanded and the array elements were structured under objects. Destructuring was used to simplify and simplify the html and css file code.

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 // Массив цитат
 const quotes = [
-  "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-  "Do what you can, with what you have, where you are. - Theodore Roosevelt",
-  "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
-  "It does not matter how slowly you go as long as you do not stop. - Confucius",
-  "Believe you can and you're halfway there. - Theodore Roosevelt",
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "Success is not the key to happiness. Happiness is the key to success.",
+    author: "Albert Schweitzer",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+  {
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington",
+  },
 ];
+
 const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
 
-// Функция генерации случайной цитаты
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  console.log(randomIndex);
   const randomQuote = quotes[randomIndex];
+  const { quote, author: quoteAuthor } = randomQuote;
 
-  quoteElement.textContent = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor;
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
