13a61a42a225645673e337561a39fa4d8c277107
YarovoiMykola <yrovau3@gmail.com>
2025-04-02 16:42:02 +0300

    Favorites button icon and refactoring

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
-// Массив цитат
-const quotes = [
-  {
-    quote:
-      "The only limit to our realization of tomorrow is our doubts of today.",
-    author: "Franklin D. Roosevelt",
-  },
-  {
-    quote: "Do what you can, with what you have, where you are.",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote:
-      "Success is not the key to happiness. Happiness is the key to success.",
-    author: "Albert Schweitzer",
-  },
-  {
-    quote: "It does not matter how slowly you go as long as you do not stop.",
-    author: "Confucius",
-  },
-  {
-    quote: "Believe you can and you're halfway there.",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote: "In the middle of every difficulty lies opportunity.",
-    author: "Albert Einstein",
-  },
-  {
-    quote:
-      "Your time is limited, so don’t waste it living someone else’s life.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Life is what happens when you're busy making other plans.",
-    author: "John Lennon",
-  },
-  {
-    quote:
-      "The future belongs to those who believe in the beauty of their dreams.",
-    author: "Eleanor Roosevelt",
-  },
-  {
-    quote: "If you want to lift yourself up, lift up someone else.",
-    author: "Booker T. Washington",
-  },
-];
-
+import quotes from "./src/quotes.js";
+import {
+  showFavoriteCard,
+  hideFavoriteCard,
+  toggleFavoriteIcon,
+} from "./src/favoriteManager.js";
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleFavoriteBtn = document.getElementById("toggle-fav-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author: quoteAuthor } = randomQuote;
-
+  toggleFavoriteBtn.style.display = "inline-block";
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = quoteAuthor;
+  toggleFavoriteIcon(randomQuote.isFavorite, toggleFavoriteBtn);
+  toggleFavoriteBtn.style.display = "inline-block";
 }
 
-generateBtn.addEventListener("click", generateRandomQuote);
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteIcon(currentQuote.isFavorite, toggleFavoriteBtn);
 
+  currentQuote.isFavorite
+    ? showFavoriteCard(
+        currentQuote.quote,
+        currentQuote.author,
+        favoritesContainer
+      )
+    : hideFavoriteCard(currentQuote.quote);
+}
+generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
 generateRandomQuote();
---
+++ src/quotes.js
@@ -0,0 +1,58 @@
+// Массив цитат
+const quotes = [
+  {
+    quote:
+      "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+    isFavorite: false,
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+    isFavorite: false,
+  },
+  {
+    quote:
+      "Success is not the key to happiness. Happiness is the key to success.",
+    author: "Albert Schweitzer",
+    isFavorite: false,
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+    isFavorite: false,
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+    isFavorite: false,
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+    isFavorite: false,
+  },
+  {
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+    isFavorite: false,
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+    isFavorite: false,
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+    isFavorite: false,
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington",
+    isFavorite: false,
+  },
+];
+export default quotes;
